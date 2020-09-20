64f67d1a3e22199783be8c069a6fe1bc363f125e
Alexandru Naiman <9295849+nalexandru@users.noreply.github.com>
2020-09-20 05:21:42 +0300

    * Removed old files

--- deps/miwa/mkconf/shared.mk
+++
@@ -1,9 +0,0 @@
-#
-# Misaki Build System Shared Variables
-#
-
-SRC_ROOT := $(CURDIR)/../..
-OUTPUT_DIR := $(SRC_ROOT)/bin
-BUILD_DIR := $(SRC_ROOT)/build
-SHARED_CFLAGS := -D_REENTRANT -D__EXTENSIONS__ -Iinclude -I../../include -I../../deps/miwa/include
-SHARED_LDFLAGS := -L$(OUTPUT_DIR)
